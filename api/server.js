b25b826965d528bf5b3d69c93a5a92fd55f8d7cf
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-28 15:51:23 -0500

    added logout

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 
 const router = require('./router.js');
 const middleware = require('./middleware.js')
@@ -7,6 +8,18 @@ const server = express();
 
 middleware(server);
 
+server.use(session({
+    name: 'cessaion',
+    secret: 'Never go against a Sicilian when DEATH is on the line!',
+    cookie: {
+        maxAge:  90 *1000,
+        secure: false, // changed to true if in production
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false, //set to false, will later be dynamically changed. GDPR laws
+}));
 server.use('/api', router);
+
 
 module.exports = server;
--- users/user-router.js
+++ users/user-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const Users = require('./model.js');
 
-
+//sign up
 router.post('/register', (req,res) => {
     let user = req.body;
     const hash = bc.hashSync(req.body.password, 6);
@@ -19,7 +19,7 @@ router.post('/register', (req,res) => {
             res.status(500).json(err)
         });
 });
-
+//login
 router.post('/login', (req, res) => {
     let {username, password} = req.body;
 
@@ -27,28 +27,54 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if(user && bc.compareSync(password, user.password)){
+                req.session.user = user;
                 res.status(200).json({ message: `Hello ${user.username}`});
             }else{
                 res.status(401).json({message: "missing header"})
             }
          })
          .catch(err => {
+             console.log(err)
              res.status(500).json(err);
          })
 });
 
-router.get('/', (req,res) => {
-    if(req.headers.authorization){
-        bc.hash(req.headers.authorization, 6, (err, hash) => {
-        if(err) {
-            res.status(500).jsont({message: "something is not working"})
-        }else{
-            res.status(200).json({ hash})
-        }
-    });
+
+//log out
+router.get('/auth/logout', (req,res) => {
+    console.log(req)
+    if(req.session) {
+        console.log(req.session)
+        req.session.destroy(err => {
+            if(err){
+                console.log(err)
+                res.json({message: "Sorry you're stuck here."})
+            }else{
+                res.status(200).json({message: "Looks like you made it out this time."})
+            }
+        })
     }else{
-        res.status(400).json({message: 'YOU SHALL NOT PASS! 🧙🏼‍'})
-}
+        res.status(500).json({message: 'We ran into an error'})
+    }
 })
+
+
+//get list of users
+router.get('/', authorized, (req,res) => {
+   Users.find()
+   .then(users => res.json(users))
+   .catch(error => res.json(error))
+});
+
+
+//authorization middleware
+function authorized(req,res,next){
+    if(req.session && req.session.user){
+        console.log(req.session)
+        next()
+    }else{
+        res.status(404).json({message: "YOU SHALL NOT PASS! 🧙🏼‍"})
+    }
+}
 
 module.exports = router;
